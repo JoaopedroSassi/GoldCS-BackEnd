21734e2957d2e3de90e47be44d72e13b85cd6c04
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-03-26 18:48:28 -0300

    BaseRepository + IBaseRepository

---
+++ src/Repositories/BaseRepository.cs
@@ -0,0 +1,35 @@
+using src.Data;
+using src.Repositories.Interfaces;
+
+namespace src.Repositories
+{
+	public class BaseRepository : IBaseRepository
+	{
+		private readonly GoldCSDBContext _context;
+
+		public BaseRepository(GoldCSDBContext context)
+		{
+			_context = context;
+		}
+
+		public void Insert<T>(T entity) where T : class
+		{
+			_context.Add(entity);
+		}
+
+		public void Delete<T>(T entity) where T : class
+		{
+			_context.Remove(entity);
+		}
+
+		public void Update<T>(T entity) where T : class
+		{
+			_context.Update(entity);
+		}
+
+		public async Task<bool> SaveChangesAsync()
+		{
+			return await _context.SaveChangesAsync() > 0;
+		}
+	}
+}
---
+++ src/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,10 @@
+namespace src.Repositories.Interfaces
+{
+	public interface IBaseRepository
+    {
+        void Insert<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveChangesAsync();
+    }
+}
