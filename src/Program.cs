cba4a1007e2ee96d1818eec5109a4f5d75e82663
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-03-26 18:50:24 -0300

    Configs do Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using src.Data;
+using src.Repositories;
+using src.Repositories.Interfaces;
+using src.Services;
+using src.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<GoldCSDBContext>(x => {
-	x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLServer"));
+builder.Services.AddDbContext<GoldCSDBContext>(x =>
+{
+	x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLServer"),
+	assembly => assembly.MigrationsAssembly(typeof(GoldCSDBContext).Assembly.FullName));
 });
+
+//Dependency injections
+builder.Services.AddScoped<IBaseRepository, BaseRepository>();
+builder.Services.AddScoped<IClientService, ClientService>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
