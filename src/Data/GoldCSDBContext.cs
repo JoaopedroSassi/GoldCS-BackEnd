6c3214f28a240c50aef80122fb183cb32cf235b5
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-03-26 18:10:52 -0300

    DbContext

--- src/Data/GoldCSDBContext.cs
+++ src/Data/GoldCSDBContext.cs
@@ -1,20 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using src.Entities.Models;
 
 namespace src.Data
 {
-    public class GoldCSDBContext : DbContext
-    {
+	public class GoldCSDBContext : DbContext
+	{
 		public DbSet<Client>? Clients { get; set; }
 
-		public GoldCSDBContext (DbContextOptions<GoldCSDBContext> options)
+		public GoldCSDBContext(DbContextOptions<GoldCSDBContext> options)
 			: base(options)
 		{
 
 		}
-    }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+		}
+	}
 }
