966af6f733798f6779dc5e4afd99907266f97983
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-03-28 16:58:16 -0300

    migration

---
+++ src/Migrations/20230328195641_DisableLandlinePhone.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoldCSAPI.Migrations
+{
+    public partial class DisableLandlinePhone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LandlinePhone",
+                table: "tb_clients",
+                type: "varchar(15)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LandlinePhone",
+                table: "tb_clients",
+                type: "varchar(15)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldNullable: true);
+        }
+    }
+}
