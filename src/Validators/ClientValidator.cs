848a4afd9f8d4501017bb895e3aca9b4816f5fe1
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-03-29 15:52:41 -0300

    Add outros campos CLientValidator

--- src/Validators/ClientValidator.cs
+++ src/Validators/ClientValidator.cs
@@ -8,11 +8,23 @@ namespace src.Validators
         public ClientValidator()
 		{
 			RuleFor(x => x.Cpf)
-				.NotEmpty().WithMessage("Necessário preencher o campo CPF")
+				.NotEmpty().WithMessage("Necessário preencher o campo 'CPF'")
 				.Matches(@"^\d{3}\.\d{3}\.\d{3}\-\d{2}$").WithMessage("CPF no formato inválido - xxx.xxx.xxx-xx")
-				;
+			;
 			
-			
+			RuleFor(x => x.Name)
+				.NotEmpty().WithMessage("Necessário preencher o campo 'Nome'")
+				.Length(1, 150).WithMessage("Campo maior do que o permitido")
+			;
+
+			RuleFor(x => x.Email)
+				.NotEmpty().WithMessage("Necessário preencher o campo 'Email'")
+				.EmailAddress().WithMessage("Email no foramto inválido")
+			;
+
+			RuleFor(x => x.CellPhone)
+				.NotEmpty().WithMessage("Necessário preencher o campo 'Cell Phone'")
+			;
 		}
     }
 }
