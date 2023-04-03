51c5565b6adc885b4a88e9e5e96e254db0d67b07
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-04-03 16:12:25 -0300

    Add Category + Product - DbSet

--- src/Data/GoldCSDBContext.cs
+++ src/Data/GoldCSDBContext.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using src.Entities.Models;
+using src.Models.Entities;
 
 namespace src.Data
 {
 	public class GoldCSDBContext : DbContext
 	{
 		public DbSet<Client> Clients { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Product> Products { get; set; }
 
 		public GoldCSDBContext(DbContextOptions<GoldCSDBContext> options)
 			: base(options)
