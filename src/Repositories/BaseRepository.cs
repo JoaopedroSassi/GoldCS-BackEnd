94583350b28490d5634f246f9e67e8ede11c4a8c
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-04-04 14:49:19 -0300

    Rmv try catch - BaseRepository

--- src/Repositories/BaseRepository.cs
+++ src/Repositories/BaseRepository.cs
@@ -1,6 +1,4 @@
-using System.Net;
 using src.Data;
-using src.Exceptions;
 using src.Repositories.Interfaces;
 
 namespace src.Repositories
@@ -16,66 +14,22 @@ namespace src.Repositories
 
 		public void Insert<T>(T entity) where T : class
 		{
-			try
-			{
-				_context.Add(entity);
-			}
-			catch (System.Exception ex)
-			{
-				string innerExceptionMessage = String.Empty;
-				if (!(ex.InnerException is null))
-					innerExceptionMessage = ex.InnerException.Message;
-
-				throw new BaseException($"Erro DB - {ex.Message}", HttpStatusCode.BadRequest, typeof(BadHttpRequestException).FullName, ValidateInnerExceptionMessage(ex.InnerException));
-			}
+			_context.Add(entity);
 		}
 
 		public void Delete<T>(T entity) where T : class
 		{
-			try
-			{
-				_context.Remove(entity);
-			}
-			catch (System.Exception ex)
-			{
-				string innerExceptionMessage = String.Empty;
-				if (!(ex.InnerException is null))
-					innerExceptionMessage = ex.InnerException.Message;
-
-				throw new BaseException($"Erro DB - {ex.Message}", HttpStatusCode.BadRequest, typeof(BadHttpRequestException).FullName, ValidateInnerExceptionMessage(ex.InnerException));
-			}
+			_context.Remove(entity);
 		}
 
 		public void Update<T>(T entity) where T : class
 		{
-			try
-			{
-				_context.Update(entity);
-			}
-			catch (System.Exception ex)
-			{
-				throw new BaseException($"Erro DB - {ex.Message}", HttpStatusCode.BadRequest, typeof(BadHttpRequestException).FullName, ValidateInnerExceptionMessage(ex.InnerException));
-			}
+			_context.Update(entity);
 		}
 
 		public async Task<bool> SaveChangesAsync()
 		{
-			try
-			{
-				return await _context.SaveChangesAsync() > 0;
-			}
-			catch (System.Exception ex)
-			{
-				throw new BaseException($"Erro DB - {ex.Message}", HttpStatusCode.BadRequest, typeof(BadHttpRequestException).FullName, ValidateInnerExceptionMessage(ex.InnerException));
-			}
-		}
-
-		private static string ValidateInnerExceptionMessage(Exception innerException)
-		{
-			if (!(innerException is null))
-				return innerException.Message;
-			
-			return String.Empty;
+			return await _context.SaveChangesAsync() > 0;
 		}
 	}
 }
