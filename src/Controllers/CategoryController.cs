cc5e480087fc6962123a7abec9c0135f875c19a3
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-04-04 16:09:23 -0300

    Add GetById + Update - Category

--- src/Controllers/CategoryController.cs
+++ src/Controllers/CategoryController.cs
@@ -19,15 +19,7 @@ namespace src.Controllers
 		[HttpGet]
 		public async Task<ActionResult<CategoryDetailsDTO>> GetCategoriasAsync()
 		{
-			var categories = await _service.GetAllCategoriesAsync();
-			if (!categories.Any())
-			{
-				var ex = new Exception("Sem categorias cadastradas");
-				ex.Data.Add("StatusCode", HttpStatusCode.NotFound);
-				throw ex;
-			}
-
-			return Ok(categories);
+			return Ok(await _service.GetAllCategoriesAsync());
 		}
 
 		[HttpGet("{id:int}")]
@@ -41,6 +33,55 @@ namespace src.Controllers
 			}
 			
 			return Ok(await _service.GetCategoryByIdAsync(id));
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<string>> InsertCategoryAsync([FromBody] CategoryInsertDTO model)
+		{
+			if (!(ModelState.IsValid))
+			{
+				var ex = new Exception("Formato inválido");
+				ex.Data.Add("StatusCode", HttpStatusCode.NotFound);
+				throw ex;
+			}
+
+			await _service.InsertCategoryAsync(model);
+			return Ok("Categoria inserida");
+		}
+
+		[HttpPut("{id:int}")]
+		public async Task<ActionResult<CategoryDetailsDTO>> UpdateCategoryAsync(int id, [FromBody] CategoryUpdateDTO model)
+		{
+			if (model.CategoryID != id)
+			{
+				var ex = new Exception("ID do parâmetro diferente do ID do corpo da requisição");
+				ex.Data.Add("StatusCode", HttpStatusCode.BadRequest);
+				throw ex;
+			}
+
+			if (!(ModelState.IsValid))
+			{
+				var ex = new Exception("Formato inválido");
+				ex.Data.Add("StatusCode", HttpStatusCode.BadRequest);
+				throw ex;
+			}
+
+			await _service.UpdateCategoryAsync(model);
+			return Ok("Categoria atualizada com sucesso");
+		}
+
+		[HttpDelete("{id:int}")]
+		public async Task<ActionResult<string>> DeleteCategoryAsync(int id) 
+		{
+			if (id <= 0)
+			{
+				var ex = new Exception("ID menor ou igual a 0");
+				ex.Data.Add("StatusCode", HttpStatusCode.NotFound);
+				throw ex;
+			}
+
+			await _service.DeleteCategoryAsync(id);
+			return Ok("Categoria deletada");
 		}
 	}
 }
