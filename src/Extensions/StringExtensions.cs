b5768748bfcbae212351f4ee5e5c507fa9320cb0
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-04-06 14:27:04 -0300

    Add stringextensions + updt ClientValidator - REGEX

---
+++ src/Extensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+using System.Text.RegularExpressions;
+
+namespace src.Extensions
+{
+	public static class StringExtensions
+    {
+        public static bool IsCpfValid(this string cpf)
+        {
+            return Regex.Match(cpf, @"^\d{3}\.\d{3}\.\d{3}-\d{2}$").Success;
+        }
+
+        public static bool IsCepValid(this string cep)
+        {
+            return Regex.Match(cep, @"^\d{5}-\d{3}$").Success;
+        }
+
+        public static bool IsPasswordValid(this string pass)
+        {
+            return Regex.Match(pass, @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$").Success;
+        }
+
+        public static bool IsEmailValid(this string email)
+        {
+            return Regex.Match(email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").Success;
+        }
+    }
+}
--- src/Validators/ClientValidator.cs
+++ src/Validators/ClientValidator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using src.Entities.DTO.Client;
+using src.Extensions;
 
 namespace src.Validators
 {
@@ -8,22 +9,29 @@ namespace src.Validators
         public ClientValidator()
 		{
 			RuleFor(x => x.Cpf)
-				.NotEmpty().WithMessage("Necessário preencher o campo 'CPF'")
-				.Matches(@"^\d{3}\.\d{3}\.\d{3}\-\d{2}$").WithMessage("CPF no formato inválido - xxx.xxx.xxx-xx")
+				.NotEmpty()
+					.WithMessage("Necessário preencher o campo 'CPF'")
+				.Must(x => x.IsCpfValid())
+					.WithMessage("CPF no formato inválido - xxx.xxx.xxx-xx")
 			;
 			
 			RuleFor(x => x.Name)
-				.NotEmpty().WithMessage("Necessário preencher o campo 'Nome'")
-				.Length(1, 150).WithMessage("Campo maior do que o permitido")
+				.NotEmpty()
+					.WithMessage("Necessário preencher o campo 'Nome'")
+				.Length(1, 150)
+					.WithMessage("Campo maior do que o permitido")
 			;
 
 			RuleFor(x => x.Email)
-				.NotEmpty().WithMessage("Necessário preencher o campo 'Email'")
-				.EmailAddress().WithMessage("Email no foramto inválido")
+				.NotEmpty()
+					.WithMessage("Necessário preencher o campo 'Email'")
+				.Must(x => x.IsEmailValid())
+					.WithMessage("Email no foramto inválido")
 			;
 
 			RuleFor(x => x.CellPhone)
-				.NotEmpty().WithMessage("Necessário preencher o campo 'Cell Phone'")
+				.NotEmpty()
+					.WithMessage("Necessário preencher o campo 'Cell Phone'")
 			;
 		}
     }
