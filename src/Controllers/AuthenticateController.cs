a561da48aa8f27ef28b9999b16dbc6db9d8464ee
JoaopedroSassi <joaopedro10813@gmail.com>
2023-05-14 00:08:49 -0300

    Rename controllers - JWT

---
+++ src/Controllers/AuthenticateController.cs
@@ -0,0 +1,53 @@
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using src.Extensions;
+using src.Models.DTO.User;
+using src.Services.Interfaces;
+
+namespace src.Controllers
+{
+	[ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticateController : ControllerBase
+    {
+		private readonly IUserService _userService;
+
+		public AuthenticateController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[AllowAnonymous]
+		[HttpPost("LoginUser")]
+		public async Task<ActionResult<string>> Login([FromBody] UserLoginDTO model)
+		{
+			if (!ModelState.IsValid)
+				ExceptionExtensions.ThrowBaseException("Formato inválido", HttpStatusCode.BadRequest);
+
+			return await _userService.Login(model); 
+		}
+
+		[Authorize]
+		[HttpPost("RegisterUser")]
+		public async Task<ActionResult<string>> Register([FromBody] UserRegisterDTO model)
+		{
+			if (!ModelState.IsValid)
+				ExceptionExtensions.ThrowBaseException("Formato inválido", HttpStatusCode.BadRequest);
+
+			await _userService.RegisterUser(model);
+			return Ok("Usuário inserido com sucesso");
+		}
+
+		[Authorize(Roles = "admin")]
+		[HttpDelete("Delete/{id:int}")]
+		public async Task<ActionResult<string>> DeleteUser(int id)
+		{
+			if (id <= 0)
+				ExceptionExtensions.ThrowBaseException("ID menor ou igual a 0", HttpStatusCode.NotFound);
+
+			await _userService.DeleteUser(id);
+			return Ok("Usuário deletado");
+		}
+    }
+}
