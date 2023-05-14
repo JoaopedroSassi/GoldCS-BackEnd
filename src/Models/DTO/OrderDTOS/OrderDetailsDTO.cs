1cebe38b8313fc8de1d11b92cb1c7be164453d29
JoaopedroSassi <joaopedro10813@gmail.com>
2023-05-14 13:44:48 -0300

    Add campos - OrderDetailsDTO e OrderInsertDTO

--- src/Models/DTO/OrderDTOS/OrderDetailsDTO.cs
+++ src/Models/DTO/OrderDTOS/OrderDetailsDTO.cs
@@ -1,12 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using src.Entities.Models;
+using src.Models.Entities;
 
 namespace src.Models.DTO.OrderDTOS
 {
-    public class OrderDetailsDTO
+	public class OrderDetailsDTO
     {
-        
-    }
+        public int OrderID { get; set; }
+		public DateTime OrderDate { get; set; }
+		public string PaymentMethod { get; set; }
+		public decimal Total { get; set; }
+		public DateTime DeliveryForecast { get; set; }
+		public int UserID { get; set; }
+		public string UserEmail { get; set; }
+		public string UserName { get; set; }
+		public Address Address { get; set; }
+		public Client Client { get; set; }
+		public List<OrderProduct> OrderProducts { get; set; } = new();
+
+		public OrderDetailsDTO(int orderID, DateTime orderDate, string paymentMethod, decimal total, DateTime deliveryForecast, int userID, string userEmail, string userName, Address address, Client client)
+		{
+			OrderID = orderID;
+			OrderDate = orderDate;
+			PaymentMethod = paymentMethod;
+			Total = total;
+			DeliveryForecast = deliveryForecast;
+			UserID = userID;
+			UserEmail = userEmail;
+			UserName = userName;
+			Address = address;
+			Client = client;
+		}
+
+		public OrderDetailsDTO()
+		{
+		}
+	}
 }
--- src/Models/DTO/OrderDTOS/OrderInsertDTO.cs
+++ src/Models/DTO/OrderDTOS/OrderInsertDTO.cs
@@ -1,9 +1,32 @@
-using src.Models.Entities;
+using src.Entities.DTO.ClientDTOS;
+using src.Models.DTO.AddressDTOS;
+using src.Models.DTO.OrderProductDTOS;
 
 namespace src.Models.DTO.OrderDTOS
 {
 	public class OrderInsertDTO
-    {
-        public User user {get; set;}
-    }
+	{
+		public string PaymentMethod { get; set; }
+		public decimal Total { get; set; }
+		public DateTime DeliveryForecast { get; set; }
+		public ClientInsertDTO Client { get; set; }
+		public AddressInsertDTO Address { get; set; }
+		public int UserID { get; set; }
+		public List<OrderProductInsertDTO> OrderProducts { get; set; } = new();
+
+		public OrderInsertDTO(string paymentMethod, decimal total, DateTime deliveryForecast, ClientInsertDTO client, AddressInsertDTO address, int userID, List<OrderProductInsertDTO> orderProducts)
+		{
+			PaymentMethod = paymentMethod;
+			Total = total;
+			DeliveryForecast = deliveryForecast;
+			Client = client;
+			Address = address;
+			UserID = userID;
+			OrderProducts = orderProducts;
+		}
+
+		public OrderInsertDTO()
+		{
+		}
+	}
 }
