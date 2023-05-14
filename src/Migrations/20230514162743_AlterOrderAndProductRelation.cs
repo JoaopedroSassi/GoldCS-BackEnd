4ef38cf6824980ce95e416bbad9ca00d76583867
JoaopedroSassi <joaopedro10813@gmail.com>
2023-05-14 18:30:20 -0300

    Migration

--- src/Migrations/20230514162743_AlterOrderAndProductRelation.cs
+++ src/Migrations/20230514162743_AlterOrderAndProductRelation.cs
@@ -8,38 +8,12 @@ namespace GoldCSAPI.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_tb_order_product_tb_order_OrderID1",
-                table: "tb_order_product");
-
-            migrationBuilder.DropIndex(
-                name: "IX_tb_order_product_OrderID1",
-                table: "tb_order_product");
-
-            migrationBuilder.DropColumn(
-                name: "OrderID1",
-                table: "tb_order_product");
-        }
+            
+		}
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "OrderID1",
-                table: "tb_order_product",
-                type: "integer",
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_tb_order_product_OrderID1",
-                table: "tb_order_product",
-                column: "OrderID1");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_tb_order_product_tb_order_OrderID1",
-                table: "tb_order_product",
-                column: "OrderID1",
-                principalTable: "tb_order",
-                principalColumn: "OrderID");
+            
         }
     }
 }
