ac517e2b75a51b733e917ecc206da0bb018d0aa5
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2025-03-30 00:29:46 -0300

    login - cleaning program.cs and creating new architecture projects

---
+++ src/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,128 @@
+﻿using src.Repositories.Interfaces;
+using src.Repositories;
+using src.Services.Interfaces;
+using src.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using src.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldCS.API.Configurations
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection AddDependencyInjectionConfiguration(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository, BaseRepository>();
+
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IMailService, MailService>();
+
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IClientService, ClientService>();
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderService, OrderService>();
+
+            services.AddScoped<IAddressRepository, AddressRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var key = Encoding.ASCII.GetBytes(configuration["Jwt:key"]);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GoldCS.API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header usando o esquema Bearer." +
+                                  "\r\n\r\nDigite 'Bearer' [espaço] e seu token no campo abaixo." +
+                                  "\r\n\r\nExemplo: \"Bearer 12345abcdef\""
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+        }
+        
+        public static void AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GoldCSDBContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultPostgreSQL"),
+                assembly => assembly.MigrationsAssembly(typeof(GoldCSDBContext).Assembly.FullName));
+            });
+
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public static void AddCorsConfiguration(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,133 +1,27 @@
-using System.Text;
 using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using src.Data;
+using GoldCS.API.Configurations;
 using src.Extensions;
 using src.Middlewares;
-using src.Repositories;
-using src.Repositories.Interfaces;
-using src.Services;
-using src.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssembly(typeof(Program).Assembly));
+builder.Services
+	.AddControllers()
+	.AddFluentValidation(x => x.RegisterValidatorsFromAssembly(typeof(Program).Assembly));
+
 builder.Services.AddEndpointsApiExplorer();
-
-//Swagger
-builder.Services.AddSwaggerGen(c =>
-{
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "GoldCS.API", Version = "v1" });
-
-	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-	{
-		Name = "Authorization",
-		Type = SecuritySchemeType.ApiKey,
-		Scheme = "Bearer",
-		BearerFormat = "JWT",
-		In = ParameterLocation.Header,
-		Description = "JWT Authorization header using the Bearer scheme." +
-		" \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below." +
-		"\r\n\r\nExample: \"Bearer 12345abcdef\"",
-	});
-	c.AddSecurityRequirement(new OpenApiSecurityRequirement
-	{
-		{
-			new OpenApiSecurityScheme
-			{
-				Reference = new OpenApiReference
-				{
-					Type = ReferenceType.SecurityScheme,
-					Id = "Bearer"
-				}
-			},
-			new string[] {}
-		}
-	});
-});
-
-// JWT
-var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:key"]);
-builder.Services.AddAuthentication(x =>
-{
-	x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-	.AddJwtBearer(x =>
-	{
-		x.RequireHttpsMetadata = false;
-		x.SaveToken = true;
-		x.TokenValidationParameters = new TokenValidationParameters
-		{
-			ValidateIssuerSigningKey = true,
-			IssuerSigningKey = new SymmetricSecurityKey(key),
-			ValidateIssuer = false,
-			ValidateAudience = false
-		};
-	});
-
-var connectionString = ConnectionExtension.GetConnectionString(builder.Configuration.GetConnectionString("DefaultPostgreSQL"));
-
-/*var connectionString = builder.Environment.IsProduction() ? builder.Configuration.GetConnectionString("Railway") : builder.Configuration.GetConnectionString("DefaultPostgreSQL");
-
-//
-
-if (builder.Environment.IsProduction())
-	connectionString = ConnectionExtension.BuildConnectionString(connectionString);*/
-
-//Database
-builder.Services.AddDbContext<GoldCSDBContext>(x =>
-{
-	//x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLServer"),
-	x.UseNpgsql(connectionString,
-	assembly => assembly.MigrationsAssembly(typeof(GoldCSDBContext).Assembly.FullName));
-});
-
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-// Cors
-builder.Services.AddCors(options =>
-{
-	options.AddDefaultPolicy(
-		policy =>
-		{
-			policy.AllowAnyOrigin()
-				.AllowAnyHeader()
-				.AllowAnyMethod();
-		});
-});
-
-//Dependency injections
-builder.Services.AddScoped<IBaseRepository, BaseRepository>();
-
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IMailService, MailService>();
-
-builder.Services.AddScoped<IClientRepository, ClientRepository>();
-builder.Services.AddScoped<IClientService, ClientService>();
-
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-
-builder.Services.AddScoped<IAddressRepository, AddressRepository>();
-
-//builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddSwaggerConfiguration();
+builder.Services.AddJwtConfiguration(builder.Configuration);
+builder.Services.AddDependencyInjectionConfiguration();
+builder.Services.AddDatabaseConfiguration(builder.Configuration);
+builder.Services.AddCorsConfiguration();
 
 var app = builder.Build();
-var scope = app.Services.CreateScope();
-await DataExtension.ManageDataAsync(scope.ServiceProvider);
+
+using (var scope = app.Services.CreateScope())
+{
+    await DataExtension.ManageDataAsync(scope.ServiceProvider);
+}
 
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
@@ -135,7 +29,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 	app.UseSwaggerUI();
 }
 
-app.UseMiddleware(typeof(GlobalErrorHandlingMiddleware));
+app.UseMiddleware<GlobalErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
